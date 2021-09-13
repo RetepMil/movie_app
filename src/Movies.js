b0df563353e791a466c11019b3e83c7ebaf279d5
hyeongwoo.lim <lkslyj2@naver.com>
2021-09-13 12:59:46 +0900

    Final2

--- src/Movies.js
+++ src/Movies.js
@@ -6,17 +6,17 @@ function Movie({ year, title, summary, poster, genres }) {
   return (
     <div className="movie">
       <img src={poster} alt={title} title={title} />
-      <div className="movie_data">
-        <h3 className="movie_title">{title}</h3>
-        <h5 className="movie_year">{year}</h5>
+      <div className="movie__data">
+        <h3 className="movie__title">{title}</h3>
+        <h5 className="movie__year">{year}</h5>
         <ul className="genres">
           {genres.map((genre, index) => (
-            <li key={index} className="genres_genre">
+            <li key={index} className="movie__genres">
               {genre}
             </li>
           ))}
         </ul>
-        <p className="movie_summary">{summary.slice(0, 140) + "..."}</p>
+        <p className="movie__summary">{summary.slice(0, 140) + "..."}</p>
       </div>
     </div>
   );
